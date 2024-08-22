24d2b7182828a6dc1f340a51d505931c973b1b0d
Md Fahim Chowdhury <163567991+fahim-56@users.noreply.github.com>
2024-08-22 12:36:30 +0600

    Add files via upload

---
+++ variable_print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    float b = 1.25;
+    char c = 'A';
+    printf("%d\n", a);
+    printf("%.2f\n", b);
+    printf("%c\n", c);
+
+    return 0;
+}
